1b15bfaea3cc6f35413af9809dec596d5a2fbbbf
MatiasJMB <matias.moreno.b@alumnos.upla.cl>
2022-10-05 21:48:20 -0300

    ultimo update

--- ejercicio_2.py
+++ ejercicio_2.py
@@ -1,20 +1,19 @@
-# Ejercicio 2 (18 puntos): Los tramos impositivos para la declaración de la renta en un determinado país son los siguientes: (IMAGEN ADJUNTA). 
+# Ejercicio 2 (18 puntos): Los tramos impositivos para la declaración de la renta en un determinado país son los siguientes: (IMAGEN ADJUNTA). Escribir un programa que pregunte al usuario su renta anual y muestre por pantalla lo que tiene que pagar.
 
 def main():
     renta = int(input("# Ingresar renta anual: "))
     if (renta < 10000):
         impuesto = (renta * 5) / 100
-    elif (renta > 10000 and renta < 20000):
+    elif (renta >= 10000 and renta < 20000):
         impuesto = (renta * 15) / 100
-    elif (renta > 20000 and renta < 35000):
+    elif (renta >= 20000 and renta < 35000):
         impuesto = (renta * 20) / 100
-    elif (renta > 35000 and renta < 60000):
+    elif (renta >= 35000 and renta < 60000):
         impuesto = (renta * 30) / 100
     else:
         impuesto = (renta * 45) / 100
 
-    print("Total a pagar: " + impuesto)
-
+    print("Valor renta a pagar: " + str(impuesto))
 
 if __name__ == '__main__':
     main()
--- ejercicio_5.py
+++ ejercicio_5.py
@@ -1,19 +1,24 @@
 # Ejercicio 5 (18 puntos): Escribir una función que calcule el total de una factura tras aplicarle el IVA. La función debe recibir la cantidad sin IVA y el porcentaje de IVA a aplicar, y devolver el total de la factura. Si se invoca la función sin pasarle el porcentaje de IVA, deberá aplicar un 19%.
 
 def iva(cantidad_sin_iva, porcentaje):
-    
-    
-    
-    pass
+    if (porcentaje == ''):
+        porcentaje = 19
+        porcentaje = int(porcentaje)
 
+    porcentaje = int(porcentaje)
+    valor_iva = cantidad_sin_iva * (porcentaje / 100)
+    total = valor_iva + cantidad_sin_iva
+    return total
 
 def main():
-    
-    
-    pass
+    valor_neto = int(input("Ingrese total NETO de la factura: "))
+    porcentaje_iva = str(input("Ingrese el porcentaje del IVA (EJ: 20, sin '%'): "))
+    resultado = iva(valor_neto,porcentaje_iva)
+    print("El total BRUTO de la factura es: $" + str(resultado))
+
 
 if __name__ == '__main__':
-    pass
+    main()
 
 '''
 ¯\_( ͡° ͜ʖ ͡°)_/¯ Matias Moreno Barrios
